b5eb6432578bfa90cee170a5c18d5a5b0403106f
Brian Hernandez <hernandezbriannnn@gmail.com>
2022-06-07 21:04:39 -0700

    Finishing Touches

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
+alert('Keep in mind this story will be about a dog.');
 const adj = prompt('Please put an adjective.');
 const vrb = prompt('Please put a verb.');
 const noun = prompt('Please put a noun');
 
-const sentence = `<h2 style="color: white; text-align: center; padding: 45vh 20vw 0px 20vw; ">There was a very ${adj} person and they ${vrb} and they went to the ${noun}.</h2>`;
+const sentence = `<h2 style="color: white; text-align: center; padding: 45vh 20vw 0px 20vw; ">There once was a ${adj} dog who wanted to ${vrb} to the ${noun}.</h2>`;
 
 document.querySelector('main').innerHTML = sentence ;
